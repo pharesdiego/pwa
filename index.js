244198cce384cbd1be30bd85172c95c0247a84c2
Diego Phares <pharesdiego@gmail.com>
2018-03-12 09:52:04 -0430

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+if('serviceWorker' in navigator){
+  
+navigator.serviceWorker
+  .register('sw.js')
+    .then(() => {
+      console.log('registered sw')
+    })
+    .catch(err => {
+      console.log('error installing sw', err)
+    })
+}
---
+++ sw.js
@@ -0,0 +1,43 @@
+var CACHE_NAME = 'italian-coders-v1';
+
+var FILES = [
+  './index.html',
+  './index.js',
+  './style.css',
+  './manifest.json',
+  'https://fonts.googleapis.com/css?family=Permanent+Marker|Roboto',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(
+        cache => cache.addAll(FILES)
+      )
+  )
+
+  // update caches
+  caches.keys().then(cachesName => cachesName.map(name => {
+    if(name != CACHE_NAME) {
+      return caches.delete(name);
+    }
+  }))
+
+})
+
+
+
+// fetch files from cache
+self.addEventListener('fetch', event => {
+
+  console.log(request)
+  event.respondWith(
+    caches.match(event.request)
+      .then(
+        response => (response) ? response : fetch(event.request)
+      )
+  )
+
+})
